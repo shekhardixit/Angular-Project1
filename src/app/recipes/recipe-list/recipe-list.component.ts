14a08f62539a847cff58c736ddb631821433ae50
shekhardixit <500067690@stu.upes.ac.in>
2021-05-29 01:19:31 +0530

    Added component to the Recipes Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[]= [
+    new Recipe('A test recipe','This is simple a test', 'https://www.google.com/search?q=recipe&sxsrf=ALeKk0320HP6rRt_hMgTNQo1P1bKsvi_yQ:1622230590971&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiw8fjwj-3wAhXv6XMBHTmaA4YQ_AUoA3oECAEQBQ&biw=1536&bih=722#imgrc=SnEqdOzPfKvxYM')
+  ];
+  
 
   constructor() { }
 
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -5,6 +5,6 @@ export class Recipe{
     constructor(name:string, desc:string,imagePath:string){
         this.name=name;
         this.description=desc;
-        this.imagePath:imagePath;
+        this.imagePath=imagePath;
     }
 }
