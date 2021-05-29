0a085dd7135ef249bf70237b156bcb2b65914f85
shekhardixit <500067690@stu.upes.ac.in>
2021-05-29 18:18:49 +0530

    Created and outputted the Shopping List

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,6 +1,5 @@
-export class Ingredients{
+export class Ingredient{
     constructor(public name:string, public amount:number){
-        this.name= name;
-        this.amount= amount;
+        
     }
 }
